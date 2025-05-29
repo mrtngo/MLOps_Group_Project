6ec9b545099da83319dbeb5789c12912418cfef6
santirhbf <santirhbf@student.ie.edu>
2025-05-29 17:08:04 +0200

    pushing more

--- src/mlops/preproccess/preproccessing.py
+++ src/mlops/preproccess/preproccessing.py
@@ -3,15 +3,19 @@ from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import SMOTE
 import pandas as pd
 from typing import Dict
+from src.mlops.data_validation.data_validation import load_config
 
-def select_features(config: Dict, df, feature_cols):
+config = load_config("config.yaml")
+params = config.get("feature_engineering", {}).get("feature_selection", {}).get("params", {})
+
+def select_features(df, feature_cols):
     X = df[feature_cols]
     y = df[config.get("target")]
-    rf = RandomForestRegressor(n_estimators=20, random_state=42)
+    rf = RandomForestRegressor(n_estimators=params["n_estimators"], random_state=params["random_state"])
     rf.fit(X, y)
     importance = rf.feature_importances_
     ranked = sorted(zip(feature_cols, importance), key=lambda x: x[1], reverse=True)
-    selected_cols = [x[0] for x in ranked[:config.get("top_n")]]
+    selected_cols = [x[0] for x in ranked[:config.get("feature_engineering", {}).get("feature_selection", {}).get("top_n")]]
     print(f"Selected Features: {selected_cols}")
     return selected_cols
 
@@ -31,7 +35,7 @@ def smote_oversample(X, y):
     if ratio > 1.5:
         print(f"Class counts: 0={class_counts.get(0,0)}, 1={class_counts.get(1,0)}")
         print("Applied SMOTE-like oversampling.")
-        sm = SMOTE(sampling_strategy='auto', random_state=42)
+        sm = SMOTE(sampling_strategy='auto', random_state=params["random_state"])
         X_res, y_res = sm.fit_resample(X, y)
     else:
         X_res, y_res = X, y
